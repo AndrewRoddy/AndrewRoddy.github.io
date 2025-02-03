b3b63edeb12f8dd9030df7407d78421ba7ed3df4
Andrew Roddy <andrewroddy7@gmail.com>
2025-02-03 08:11:59 -0500

    0.12.01 Adds cursor glow

--- js/index.js
+++ js/index.js
@@ -1,4 +1,23 @@
+const cursor = document.createElement('div');
+cursor.classList.add('glow-cursor');
+document.body.appendChild(cursor);
 
-document.addEventListener('DOMContentLoaded', function() {
-
+// Makes the glow follow the mouse
+document.addEventListener('mousemove', (e) => {
+    cursor.style.left = `${e.clientX}px`;
+    cursor.style.top = `${e.clientY}px`;
 });
+
+document.addEventListener('mouseenter', () => {
+    cursor.style.width = '200px';
+    cursor.style.height = '200px';
+    cursor.style.boxShadow = ' 0 0 300px rgb(255, 255, 255, 1)';
+});
+
+document.addEventListener('mouseleave', () => {
+    cursor.style.width = '0'; /* Shrinks the size */
+    cursor.style.height = '0'; /* Shrinks the size */
+    /*cursor.style.boxShadow = '0 0 0px rgb(255, 255, 255, 0.25)'*/
+    /*cursor.style.backgroundColor = '0 0 10px rgb(0, 0, 0)';*/
+});
+
