25698261f27542101f259e713cfbe76bcffc3976
Andrew Roddy <andrewroddy7@gmail.com>
2025-02-04 12:10:49 -0500

    0.12.10 Finalizes cursor glow

--- js/index.js
+++ js/index.js
@@ -1,23 +1,25 @@
 const cursor = document.createElement('div');
 cursor.classList.add('glow-cursor');
+cursor.style.opacity = '0.0';
 document.body.appendChild(cursor);
 
 // Makes the glow follow the mouse
 document.addEventListener('mousemove', (e) => {
     cursor.style.left = `${e.clientX}px`;
     cursor.style.top = `${e.clientY}px`;
+
+    // Fix : Makes cursor glow once webpage loaded
+    cursor.style.opacity = '1.0';
 });
 
 document.addEventListener('mouseenter', () => {
+    cursor.style.opacity = '1.0';
     cursor.style.width = '200px';
     cursor.style.height = '200px';
     cursor.style.boxShadow = ' 0 0 300px rgb(255, 255, 255, 1)';
 });
 
 document.addEventListener('mouseleave', () => {
-    cursor.style.width = '0'; /* Shrinks the size */
-    cursor.style.height = '0'; /* Shrinks the size */
-    /*cursor.style.boxShadow = '0 0 0px rgb(255, 255, 255, 0.25)'*/
-    /*cursor.style.backgroundColor = '0 0 10px rgb(0, 0, 0)';*/
+    cursor.style.opacity = '0.5';
 });
 
