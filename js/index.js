a7699b14973a43f65d3390ccb6287d5ed7dcc85c
Andrew Roddy <andrewroddy7@gmail.com>
2025-02-13 08:45:45 -0500

    0.13.00 Switches to a two colum layout

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 const cursor = document.createElement('div');
 cursor.classList.add('glow-cursor');
-cursor.style.opacity = '0.5';
-cursor.style.left = '0px';
-cursor.style.top = '0px';
+
+// Opacity and position on start 
+cursor.style.opacity = '0.7';
+cursor.style.left = '20px';
+cursor.style.top = '20px';
 document.body.appendChild(cursor);
 
 // Makes the glow follow the mouse
@@ -11,17 +13,18 @@ document.addEventListener('mousemove', (e) => {
     cursor.style.top = `${e.clientY}px`;
 
     // Fix : Makes cursor glow once webpage loaded
-    cursor.style.opacity = '1.0';
+    cursor.style.opacity = '0.7';
 });
 
 document.addEventListener('mouseenter', () => {
-    cursor.style.opacity = '1.0';
+    cursor.style.opacity = '0.7';
     cursor.style.width = '200px';
     cursor.style.height = '200px';
     cursor.style.boxShadow = ' 0 0 300px rgb(255, 255, 255, 1)';
 });
 
-document.addEventListener('mouseleave', () => {
-    cursor.style.opacity = '0.5';
-});
+// Can be used to make cursor dimmer when starting
+//document.addEventListener('mouseleave', () => {
+//    cursor.style.opacity = '0.5';
+//});
 
